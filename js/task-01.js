2f75a54f5188ecace8082a64d64cd2908285d8d4
ishkanderka <victoriashmidt@outlook.com>
2023-04-17 15:53:35 -0600

    First task done ✔️

---
+++ js/task-01.js
@@ -0,0 +1,7 @@
+const categoriesNumber = document.querySelectorAll(".item");
+console.log("Number of categories: ", categoriesNumber.length);
+
+categoriesNumber.forEach((item) => {
+  console.log("Category: ", item.firstElementChild.textContent);
+  console.log("Elements:", item.querySelectorAll("li").length);
+});
