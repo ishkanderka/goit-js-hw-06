1ed464d1abcd90faed107d15737bccccff256d3c
ishkanderka <victoriashmidt@outlook.com>
2023-04-18 22:36:54 -0600

    Task 2 done 👩🏻‍💻

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,23 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListEl = document.querySelector("#ingredients");
+
+const createNewEl = (elements) => {
+  return elements.map((element) => {
+    const newEl = document.createElement("li");
+    newEl.classList.add("item");
+    newEl.textContent = element;
+
+    return newEl;
+  });
+};
+
+const items = createNewEl(ingredients);
+ingredientsListEl.append(...items);
