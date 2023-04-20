6b10394a8ddfaeefbdfffab4652b06f793dfff03
ishkanderka <victoriashmidt@outlook.com>
2023-04-20 10:43:54 -0600

    Task 8 done 👩🏻‍💻

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
+form.addEventListener("submit", handleSubmit);
