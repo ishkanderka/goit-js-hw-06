9b0f35008cf35936e18c9f03134af9acaa8704c3
ishkanderka <victoriashmidt@outlook.com>
2023-04-20 11:09:11 -0600

    Task 9 done 👩🏻‍💻

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+const body = document.querySelector("body");
+const colorValue = document.querySelector(".color");
+const btn = document.querySelector(".change-color");
+
+function onBtnClick(event) {
+  event.preventDefault();
+  body.style.backgroundColor = getRandomHexColor();
+  colorValue.innerHTML = body.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+btn.addEventListener("click", onBtnClick);
