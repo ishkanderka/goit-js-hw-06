0084a76b907d424c2339a62ba9934e4ccc92f4ad
ishkanderka <victoriashmidt@outlook.com>
2023-04-20 21:47:13 -0600

    Changed condition

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,8 @@ const textInput = document.querySelector("#validation-input");
 const textLength = textInput.getAttribute("data-length");
 
 const inputElValidation = (event) => {
-  return event.currentTarget.value.length <= textLength
+  return event.currentTarget.value.length == textLength
     ? (textInput.style.borderColor = "#4caf50")
     : (textInput.style.borderColor = "#f44336");
 };
-
 textInput.addEventListener("blur", inputElValidation);
