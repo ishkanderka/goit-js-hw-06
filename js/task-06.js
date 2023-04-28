4948d99a88ef4a2e85e8572c4ba72fa6901e90a9
ishkanderka <victoriashmidt@outlook.com>
2023-04-28 14:59:32 -0600

    Bug fix

--- js/task-06.js
+++ js/task-06.js
@@ -3,7 +3,7 @@ const textLength = textInput.getAttribute("data-length");
 
 const inputElValidation = (event) => {
   return event.currentTarget.value.length == textLength
-    ? (textInput.style.borderColor = "#4caf50")
-    : (textInput.style.borderColor = "#f44336");
+    ? (textInput.classList = "valid")
+    : (textInput.classList = "invalid");
 };
 textInput.addEventListener("blur", inputElValidation);
